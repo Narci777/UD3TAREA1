638fcbcb6916b760294c5106d37bf546f3d81c29
NARCISO <nabadb01@educantabria.es>
2025-11-24 10:06:59 +0100

    1

---
+++ src/main/java/org/example/Libro.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Libro {
+
+    public void Libro  (){
+        this.titulo=JacksonelCap;
+        this.autor=Leonardo;
+        this.numerodepaginas=105;
+        this.ISBN=12345;
+
+    }
+    public void Libro(String titulo,  String autor, int numerodepaginas, int ISBN ){
+        this.titulo=titulo;
+        this.autor=autor;
+        this.numerodepaginas=numerodepaginas;
+        this.ISBN=ISBN;
+
+    }
+
+}
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Creo un Nuevo Libro
+
+        Libro L1 = new Libro();
+
+        Libro L2 = new Libro();
+
+        Libro L3 = new Libro();
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
